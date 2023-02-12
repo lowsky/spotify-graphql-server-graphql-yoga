5c7548306c84fd990cfde22beebae0bd9a751c2c
lowsky <lowsky@users.noreply.github.com>
2023-02-12 14:06:18 +0100

    migrate to graphql-yoga

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,52 +1,12 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-import { getGraphQLParameters, processRequest, renderGraphiQL, sendResult, shouldRenderGraphiQL } from "graphql-helix";
-import { envelop, useEngine, useSchema } from "@envelop/core"
-import { execute, parse, subscribe, validate } from 'graphql';
+import { createYoga } from "graphql-yoga";
 
-import {schema} from "../data/schema";
+import { schema } from "../data/schema";
 
-const getEnveloped = envelop({
-    enableInternalTracing: true,
-    plugins: [
-        useSchema(schema),
-        useEngine({ parse, validate, execute, subscribe })
-    ],
+export default createYoga<{
+    req: VercelRequest, res: VercelResponse
+}>({
+    graphqlEndpoint: '/api/graphql',
+    schema
 })
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-    const request = {
-        body: req.body,
-        headers: req.headers,
-        method: req.method ?? 'POST',
-        query: req.query
-    }
-
-    // Determine whether we should render GraphiQL instead of returning an API res
-    if (shouldRenderGraphiQL(request)) {
-        res.setHeader("Content-Type", "text/html");
-        res.send(renderGraphiQL({
-            endpoint: '/api/graphql',
-        }));
-        return;
-    }
-
-    const {parse, validate, contextFactory, execute, schema} = getEnveloped({})
-
-    const {operationName, query, variables} = getGraphQLParameters(request)
-
-    const result = await processRequest({
-        parse,
-        validate,
-        execute,
-        contextFactory,
-
-        operationName,
-        query,
-        variables,
-        request,
-        schema,
-    })
-
-    await sendResult(result, res);
-};
--- app.ts
+++ app.ts
@@ -1,71 +1,22 @@
+import { createYoga } from "graphql-yoga"
 import express from "express";
 import path from "path";
 import cors from "cors";
 
-import { getGraphQLParameters, processRequest, renderGraphiQL, sendResult, shouldRenderGraphiQL } from "graphql-helix";
-import { envelop, useEngine, useSchema } from "@envelop/core"
-import { execute, parse, subscribe, validate } from 'graphql';
+import { schema } from "./data/schema";
 
-import {schema} from "./data/schema";
-import { fetchArtistsByName } from "./data/resolvers";
-
-const getEnveloped = envelop({
-  enableInternalTracing: true,
-  plugins: [
-    useSchema(schema),
-    useEngine({ parse, validate, execute, subscribe })
-  ],
+const yoga = createYoga({
+  schema,
+  graphqlEndpoint: '/api/graphql',
+  batching: true,
+  logging: false
 })
-
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join('.', 'public')));
 
-const rootValue = {
-  hi: () => 'Hello world!',
-  queryArtists: ({ byName }: { byName: string }) => fetchArtistsByName(byName)
-};
-
-// API middleware
-
-app.use('/api/graphql', cors(), async function (req, res) {
-    const request = {
-      body: req.body,
-      headers: req.headers,
-      method: req.method,
-      query: req.query
-    }
-
-    // Determine whether we should render GraphiQL instead of returning an API response
-    if (shouldRenderGraphiQL(request)) {
-      res.header("Content-Type", "text/html");
-      res.send(renderGraphiQL({
-        endpoint: '/api/graphql',
-      }));
-      return;
-    }
-
-    const {parse, validate, contextFactory, execute, schema} = getEnveloped({})
-
-    const {operationName, query, variables} = getGraphQLParameters(request)
-
-    const result = await processRequest({
-      parse,
-      validate,
-      execute,
-      contextFactory,
-
-      operationName,
-      query,
-      variables,
-      request,
-      schema,
-    })
-
-    await sendResult(result, res);
-  }
-);
+app.use('/api/graphql', cors(), yoga);
 
 export default app;
